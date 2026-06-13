e9586d17e25d7a2f6980da56c0ed9919cd663f57
Starky-vi <bhuvanaprasadreddy@gmail.com>
2026-06-13 20:59:22 +0800

    Implement user input for cookie selection
    Added user input for cookie quantity and validation loop.

--- hello.py
+++ hello.py
@@ -2,3 +2,9 @@
 ## seeing if i can run some basic code maybe
 
 print("Hello world!")
+user_in = int(input("How many cookies would you like?"))
+while user_in < 0 or user_in > 10:
+    print("Please make sure it's between 1 and 10!!")
+    user_in = int(input("How many cookies would you like?"))
+for i in range(user_in):
+    print("\nU+1F36A")
